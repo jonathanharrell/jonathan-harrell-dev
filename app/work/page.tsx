19373bd19630c47fc674a4c8f0f356b93a1eb0f3
jonathanharrell <harr041@gmail.com>
2024-09-28 13:26:07 -0400

    WIP

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,14 +1,46 @@
+import ReactMarkdown from "react-markdown";
 import { Header } from "@/components/header";
-import resume from "@/content/resume.json";
+import { default as resumeData } from "@/content/resume.json";
+import { Job, Project, Resume } from "@/types";
+
+const resume = resumeData as Resume;
 
 const WorkPage = async () => {
   return (
     <>
       <Header title="Work" />
-      <main>
-        <section>
-          {JSON.stringify(resume)}
-        </section>
+      <main className="space-y-12">
+        {Object.entries(resume).map(([key, section]) => (
+          <section key={key}>
+            <h2 className="font-medium">{section.title}</h2>
+            {section.job && (
+              <ul className="space-y-4 mt-4">
+                {section.job.map((job: Job) => (
+                  <li key={job.company}>
+                    <h3>{job.company}</h3>
+                    <p>{job.position}</p>
+                    <p>{job.startdate}</p>
+                    <p>{job.enddate}</p>
+                    <ReactMarkdown className="prose">{job.description}</ReactMarkdown>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {section.description && (
+              <ReactMarkdown>{section.description}</ReactMarkdown>
+            )}
+            {section.project && (
+              <ul>
+                {section.project.map((project: Project) => (
+                  <li key={project.name}>
+                    <h3>{project.name}</h3>
+                    <p>{project.link}</p>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
+        ))}
       </main>
     </>
   );
---
+++ types.ts
@@ -0,0 +1,27 @@
+export interface Job {
+  company: string;
+  position: string;
+  description: string;
+  startdate: string;
+  enddate: string;
+}
+
+export interface Project {
+  name: string;
+  link: string;
+}
+
+export interface Resume {
+  history: {
+    title: string;
+    job: Job[];
+  },
+  education: {
+    title: string;
+    description: string;
+  },
+  opensource: {
+    title: string;
+    project: Project[];
+  },
+}
