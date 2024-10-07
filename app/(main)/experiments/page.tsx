16b6cba67133f94554ebe3cfd3e5265d353ae4ea
jonathanharrell <harr041@gmail.com>
2024-10-06 20:56:35 -0400

    WIP

--- app/(main)/experiments/page.tsx
+++ app/(main)/experiments/page.tsx
@@ -9,7 +9,7 @@ const ExperimentsPage = () => {
         <section>
           <ul className="space-y-8 [&_li_a]:font-medium [&_li_a]:underline [&_li_a]:underline-offset-2 [&_li_a]:decoration-neutral-200 [&_li_a]:dark:decoration-neutral-700">
             <li>
-              <Link href="/experiments/css-grid-callouts">CSS Grid</Link>
+              <Link href="/experiments/css-grid">CSS Grid</Link>
             </li>
             <li>
               <Link href="/experiments/css-grid-callouts">CSS Grid Callouts</Link>
