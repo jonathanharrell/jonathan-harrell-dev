60f4c520c0bea9bc694bbbf48344be95dc371316
jonathanharrell <harr041@gmail.com>
2025-04-26 11:47:27 -0400

    Remove transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
-import React, {
-  ReactNode,
-  Suspense,
-  unstable_ViewTransition as ViewTransition,
-} from "react";
+import React, { ReactNode, Suspense } from "react";
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import NextTopLoader from "nextjs-toploader";
@@ -44,73 +40,71 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <ViewTransition>
-      <ThemeProvider>
-        <html lang="en" suppressHydrationWarning className="bg-neutral-950">
-          {/*Note that we should NOT use Next's <Head /> component here since this will cause a flash of the default theme*/}
-          <head>
-            <link
-              rel="preload"
-              href="/fonts/etbookot-roman-webfont.woff2"
-              as="font"
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              href="/fonts/etbookot-italic-webfont.woff2"
-              as="font"
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link rel="icon" href="/assets/favicon/favicon.ico" sizes="32x32" />
-            <link
-              rel="icon"
-              href="/assets/favicon/icon.svg"
-              type="image/svg+xml"
-            />
-            <link
-              rel="apple-touch-icon"
-              href="/assets/favicon/apple-touch-icon.png"
-            />
-            <meta name="color-scheme" content="light dark" />
-            {/*<script*/}
-            {/*  dangerouslySetInnerHTML={{*/}
-            {/*    __html: `${setInitialTheme.toString()}\n\nsetInitialTheme();`,*/}
-            {/*  }}*/}
-            {/*/>*/}
-          </head>
-          <body className="min-h-screen bg-neutral-900 font-etbook text-neutral-300">
-            <div className="container overflow-hidden">
-              <NextTopLoader color="#e6594c" />
-              <SkipToContent />
-              <Header />
-              <main
-                id="main"
-                className="flex flex-col flex-1 focus-visible:ring-0 focus-visible:ring-offset-0"
-                tabIndex={-1}
+    <ThemeProvider>
+      <html lang="en" suppressHydrationWarning className="bg-neutral-950">
+        {/*Note that we should NOT use Next's <Head /> component here since this will cause a flash of the default theme*/}
+        <head>
+          <link
+            rel="preload"
+            href="/fonts/etbookot-roman-webfont.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/etbookot-italic-webfont.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link rel="icon" href="/assets/favicon/favicon.ico" sizes="32x32" />
+          <link
+            rel="icon"
+            href="/assets/favicon/icon.svg"
+            type="image/svg+xml"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/assets/favicon/apple-touch-icon.png"
+          />
+          <meta name="color-scheme" content="light dark" />
+          {/*<script*/}
+          {/*  dangerouslySetInnerHTML={{*/}
+          {/*    __html: `${setInitialTheme.toString()}\n\nsetInitialTheme();`,*/}
+          {/*  }}*/}
+          {/*/>*/}
+        </head>
+        <body className="min-h-screen bg-neutral-900 font-etbook text-neutral-300">
+          <div className="container overflow-hidden">
+            <NextTopLoader color="#e6594c" />
+            <SkipToContent />
+            <Header />
+            <main
+              id="main"
+              className="flex flex-col flex-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+              tabIndex={-1}
+            >
+              <Suspense
+                fallback={
+                  <div className="flex items-center justify-center flex-1">
+                    <Spinner />
+                  </div>
+                }
               >
-                <Suspense
-                  fallback={
-                    <div className="flex items-center justify-center flex-1">
-                      <Spinner />
-                    </div>
-                  }
-                >
-                  {children}
-                </Suspense>
-              </main>
-              <Subscribe />
-              <Footer />
-              <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-              />
-            </div>
-          </body>
-          {gaId && <GoogleAnalytics gaId={gaId} />}
-        </html>
-      </ThemeProvider>
-    </ViewTransition>
+                {children}
+              </Suspense>
+            </main>
+            <Subscribe />
+            <Footer />
+            <script
+              type="application/ld+json"
+              dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            />
+          </div>
+        </body>
+        {gaId && <GoogleAnalytics gaId={gaId} />}
+      </html>
+    </ThemeProvider>
   );
 }
